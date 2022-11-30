00059b521431d22cb5249f829ece577719b44c72
MinderaLearningDirector <111889042+MinderaLearningDirector@users.noreply.github.com>
2022-11-30 11:26:37 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,13 @@
+// This is a single line comment
+/* this is a multiple line comment
+
+it can go for many lines.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Minders on Wednesday")
+}
